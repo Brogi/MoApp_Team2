6a614a7c8e767567ac0d5bc0e134ee6d12f75e18
fbwlsgh93 <42794341+fbwlsgh93@users.noreply.github.com>
2021-11-08 11:55:20 +0900

    전북, 수도권 map 추가

--- app/src/main/java/com/example/myapplication/MapActivity.kt
+++ app/src/main/java/com/example/myapplication/MapActivity.kt
@@ -12,6 +12,61 @@ class MapActivity : AppCompatActivity() {
 
         val entireMap = findViewById<ImageView>(R.id.entire_map)
         val mapId = arrayOf(
+            // 수도권
+            R.id.ansan,
+            R.id.anseong,
+            R.id.anyang,
+            R.id.bucheon,
+            R.id.daebudo,
+            R.id.dongducheon,
+            R.id.ganghwa,
+            R.id.gapyeong,
+            R.id.gimpo,
+            R.id.goyang,
+            R.id.gunpo,
+            R.id.guri,
+            R.id.gwacheon,
+            R.id.gwangju,
+            R.id.gwangmyeong,
+            R.id.gyodongdo,
+            R.id.hanam,
+            R.id.hwaseong,
+            R.id.icheon,
+            R.id.incheon,
+            R.id.namyangju,
+            R.id.osan,
+            R.id.paju,
+            R.id.pocheon,
+            R.id.pyeongtaek,
+            R.id.seongmodo,
+            R.id.seongnam,
+            R.id.seoul,
+            R.id.siheung,
+            R.id.suwon,
+            R.id.uijeongbu,
+            R.id.uiwang,
+            R.id.yangju,
+            R.id.yangpyeong,
+            R.id.yeoju,
+            R.id.yeoncheon,
+            R.id.yeongjongdo,
+            R.id.yongin,
+            // 전북
+            R.id.buan,
+            R.id.gimje,
+            R.id.gochang,
+            R.id.gunsan,
+            R.id.iksan,
+            R.id.imsil,
+            R.id.jangsu,
+            R.id.jeongeup,
+            R.id.jeonju,
+            R.id.jinan,
+            R.id.muju,
+            R.id.namwon,
+            R.id.sunchang,
+            R.id.wanju,
+            // 충청도
             R.id.asan,
             R.id.boeun,
             R.id.boryeong,
@@ -42,7 +97,7 @@ class MapActivity : AppCompatActivity() {
             R.id.yeongdong,
             R.id.yesan
         )
-        val mapImage = arrayOfNulls<ImageView>(29)
+        val mapImage = arrayOfNulls<ImageView>(82) // 수도권 39, 전북 14, 충청도 29
 
         for (i in mapId.indices) {
             mapImage[i] = findViewById<ImageView>(mapId[i])
