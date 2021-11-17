7ef8c0b51f47558cff30c8c1a4bf658e4784393e
jwy614 <33208278+Brogi@users.noreply.github.com>
2021-11-17 22:38:04 +0900

    item listener added

--- app/src/main/java/com/example/myapplication/StoriesAdapter.kt
+++ app/src/main/java/com/example/myapplication/StoriesAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class StoriesAdapter(private val context : Context, private  val dataList : ArrayList<ThumbNailData>) : RecyclerView.Adapter<StoriesAdapter.ItemViewHolder>() {
@@ -13,6 +16,7 @@ class StoriesAdapter(private val context : Context, private  val dataList : Arra
     inner class ItemViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         val tnImage = itemView.findViewById<ImageView>(R.id.story_thumbnailView)
         val dateText = itemView.findViewById<TextView>(R.id.story_dateTV)
+        val layout = itemView.findViewById<LinearLayout>(R.id.story_item_layout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -23,6 +27,12 @@ class StoriesAdapter(private val context : Context, private  val dataList : Arra
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.tnImage.setImageResource(dataList[position].tnImage)
         holder.dateText.text = dataList[position].date
+        holder.layout.setOnClickListener {
+            // To be removed
+            Toast.makeText(context,"Hello world!",Toast.LENGTH_SHORT).show()
+            /*var intent = Intent(context,액티비티이름::class.java)
+            startActivity(intent)*/
+        }
     }
 
     override fun getItemCount(): Int {
