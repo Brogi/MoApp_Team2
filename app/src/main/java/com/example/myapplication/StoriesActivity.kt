e7f8d98b57462161b4f6206bc0ffd527b2f880f4
jwy614 <33208278+Brogi@users.noreply.github.com>
2021-11-17 22:12:00 +0900

    Taking Local Name enabled

--- app/src/main/java/com/example/myapplication/StoriesActivity.kt
+++ app/src/main/java/com/example/myapplication/StoriesActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -10,12 +11,18 @@ import androidx.recyclerview.widget.RecyclerView
 class StoriesActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var addStoryBtn : Button
+    lateinit var localTv : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stories)
 
         recyclerView = findViewById(R.id.as_RecyclerV)
         addStoryBtn = findViewById(R.id.as_AddStoryBtn)
+        localTv = findViewById(R.id.as_LocalTV)
+
+        var intent = intent
+
+        localTv.text = intent.getStringExtra("Local")
 
         addStoryBtn.setOnClickListener {
             var intent = Intent(applicationContext,AddStoryActivity::class.java)
